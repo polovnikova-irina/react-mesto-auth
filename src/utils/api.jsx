def3680793f3f0039dbf41f8042d20c575b1ca9e
unknown <polovnikovai19i@gmail.com>
2023-08-11 16:46:40 +0300

    спринт 12 api

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,116 +1,94 @@
-  class Api {
-    constructor(options) {
-      this._url = options.baseUrl;
-      this._headers = options.headers;
-      this._authorization = options.headers.authorization;
-    }
-  
-    _checkResponce(res) {
-      return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`)
-    }
-  
-    getInfo() {
-      return fetch(`${this._url}/users/me`, {
-        headers: {
-          authorization: this._authorization
-        }
-      })
-        .then(this._checkResponce)
-    }
-  
-    getCards() {
-      return fetch(`${this._url}/cards`, {
-        headers: {
-          authorization: this._authorization
-        }
-      })
-        .then(this._checkResponce)
-    }
-  
-    sentUsersData(data) {
-      return fetch(`${this._url}/users/me`, {
+class Api {
+  constructor(options) {
+    this._baseUrl = options.baseUrl;
+    this._headers = options.headers;
+  }
+
+  _checkResponse(res) {
+    return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
+  }
+
+  _request(endpoint, options) {
+    const url = `${this._baseUrl}${endpoint}`;
+    return fetch(url, options).then(this._checkResponse);
+  }
+
+  getInfo() {
+    return this._request('/users/me', {
+      method: 'GET',
+      headers: this._headers
+    });
+  }
+
+  getCards() {
+    return this._request('/cards', {
+      method: 'GET',
+      headers: this._headers
+    });
+  }
+
+  sentUsersData(data) {
+    return this._request('/users/me', {
       method: 'PATCH',
       headers: {
-        authorization: this._authorization,
+        ...this._headers,
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-        name: data.name,
-        about: data.job
-      })
-  })
-    .then(this._checkResponce)
+      body: JSON.stringify({ name: data.name, about: data.job })
+    });
   }
-  
+
   createCard(data) {
-    return fetch(`${this._url}/cards`, {
+    return this._request('/cards', {
       method: 'POST',
       headers: {
-        authorization: this._authorization,
+        ...this._headers,
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-        name: data.name,
-        link: data.link
-      })
-  })
-    .then(this._checkResponce)
+      body: JSON.stringify({ name: data.name, link: data.link })
+    });
   }
-  
+
   addAvatar(data) {
-    return fetch(`${this._url}/users/me/avatar`, {
+    return this._request('/users/me/avatar', {
       method: 'PATCH',
       headers: {
-        authorization: this._authorization,
+        ...this._headers,
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-        avatar: data.avatar
-      })
-  })
-    .then(this._checkResponce)
+      body: JSON.stringify({ avatar: data.avatar })
+    });
   }
-  
+
   deleteCard(cardId) {
-    return fetch(`${this._url}/cards/${cardId}`, {
+    return this._request(`/cards/${cardId}`, {
       method: 'DELETE',
-      headers: {
-        authorization: this._authorization,
-      },
-  })
-    .then(this._checkResponce)
+      headers: this._headers
+    });
   }
-  
+
   addLike(cardId) {
-    return fetch(`${this._url}/cards/${cardId}/likes`, {
+    return this._request(`/cards/${cardId}/likes`, {
       method: 'PUT',
-      headers: {
-        authorization: this._authorization,
-      },
-  })
-    .then(this._checkResponce)
+      headers: this._headers
+    });
   }
-  
+
   deleteLike(cardId) {
-    return fetch(`${this._url}/cards/${cardId}/likes`, {
+    return this._request(`/cards/${cardId}/likes`, {
       method: 'DELETE',
-      headers: {
-        authorization: this._authorization,
-      },
-  })
-    .then(this._checkResponce)
+      headers: this._headers
+    });
   }
 }
 
 export const api = new Api({
-    baseUrl: "https://mesto.nomoreparties.co/v1/cohort-69",
-    headers: {
-      authorization: "9ec885fb-bc6f-4c8c-9e39-a212b12d1d1a",
-      "Content-Type": "application/json",
-    },
-  });
-
-
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-69',
+  headers: {
+    authorization: '9ec885fb-bc6f-4c8c-9e39-a212b12d1d1a',
+    'Content-Type': 'application/json',
+  },
+});
 
 
   
